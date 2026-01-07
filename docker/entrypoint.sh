5018269f86be22f48b654dad1a7f5a48c699578d
Tuan Huy <tuanhuy16903@gmail.com>
2026-01-07 13:48:13 +0700

    add create file storage in laravel

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache /var/www/database
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache /var/www/database
-
 if ! grep -q "APP_KEY=base64:" /var/www/.env; then
     php artisan key:generate --force
 fi
@@ -13,6 +10,13 @@ php artisan migrate --force
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
+
 php artisan l5-swagger:generate
+
+php artisan storage:link || true
+# truong hop storage da co roi
+
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache /var/www/database
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache /var/www/database
 
 exec /usr/bin/supervisord
