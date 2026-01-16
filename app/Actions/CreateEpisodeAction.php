59f892f0e01a6bfdec92b21889994b8f4a860465
Tuan Huy <tuanhuy16903@gmail.com>
2026-01-16 21:56:49 +0700

    add transaction for create episode

--- app/Actions/CreateEpisodeAction.php
+++ app/Actions/CreateEpisodeAction.php
@@ -6,22 +6,26 @@ use App\Models\Episode;
 use App\Models\Podcast;
 use App\Services\EpisodeUploadService;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
 
 class CreateEpisodeAction
 {
     public function handle(array $data): Episode
     {
-        if(isset($data['podcast_id'])){
-            $podcast = Podcast::where('id',$data['podcast_id'])->first();
 
-            // ham isset kiem tra
-            // 1/ co khai bao (ton tai) tuc la cot podcast_id co gia tri
-            // 2/ co bi null ko (tuc la ton tai ma ko co gia tri)
-            if (isset($data['audio_path']) && $data['audio_path'] instanceof UploadedFile) {
-                $data['audio_path'] = EpisodeUploadService::store($data['audio_path'],$podcast->title);
+        return DB::transaction(function() use ($data){
+            if(isset($data['podcast_id'])){
+                $podcast = Podcast::where('id',$data['podcast_id'])->first();
+
+                // ham isset kiem tra
+                // 1/ co khai bao (ton tai) tuc la cot podcast_id co gia tri
+                // 2/ co bi null ko (tuc la ton tai ma ko co gia tri)
+                if (isset($data['audio_path']) && $data['audio_path'] instanceof UploadedFile) {
+                    $data['audio_path'] = EpisodeUploadService::store($data['audio_path'],$podcast->title);
+                }
             }
-        }
 
-        return Episode::create($data);
+            return Episode::create($data);
+        });
     }
 }
